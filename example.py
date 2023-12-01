9ba297abcba576a42e4cff7b61f630f8da4690a2
Kevin Slagle <kjslag@gmail.com>
2023-11-30 18:05:36 -0600

    temperature

--- example.py
+++ example.py
@@ -8,6 +8,9 @@
 """Minimal standalone example to reproduce the main results from the paper
 "Elucidating the Design Space of Diffusion-Based Generative Models"."""
 
+import os
+import click
+import math
 import tqdm
 import pickle
 import numpy as np
@@ -22,12 +25,13 @@ def generate_image_grid(
     seed=0, gridw=8, gridh=8, device=None,
     num_steps=18, sigma_min=0.002, sigma_max=80, rho=7,
     S_churn=0, S_min=0, S_max=float('inf'), S_noise=1,
+    temperature=1
 ):
     batch_size = gridw * gridh
     torch.manual_seed(seed)
     if device is None:
         device = dnnlib.util.default_device()
-    ideally_float64 = torch.float64 if device.type != 'mps' else torch.float32
+    ideally_float64 = torch.float64 if device.type != 'mps' else torch.float32 # TODO check device instead
 
     # Load network.
     print(f'Loading network from "{network_pkl}"...')
@@ -36,7 +40,7 @@ def generate_image_grid(
 
     # Pick latents and labels.
     print(f'Generating {batch_size} images...')
-    latents = torch.randn([batch_size, net.img_channels, net.img_resolution, net.img_resolution], device=device)
+    latents = math.sqrt(temperature) * torch.randn([batch_size, net.img_channels, net.img_resolution, net.img_resolution], device=device)
     class_labels = None
     if net.label_dim:
         class_labels = torch.eye(net.label_dim, device=device)[torch.randint(net.label_dim, size=[batch_size], device=device)]
@@ -58,17 +62,19 @@ def generate_image_grid(
         # Increase noise temporarily.
         gamma = min(S_churn / num_steps, np.sqrt(2) - 1) if S_min <= t_cur <= S_max else 0
         t_hat = net.round_sigma(t_cur + gamma * t_cur)
+        if temperature != 1:
+            assert S_noise == 0
         x_hat = x_cur + (t_hat ** 2 - t_cur ** 2).sqrt() * S_noise * torch.randn_like(x_cur)
 
         # Euler step.
         denoised = net(x_hat, t_hat, class_labels).to(ideally_float64)
-        d_cur = (x_hat - denoised) / t_hat
+        d_cur = (x_hat - denoised) / (t_hat * temperature)
         x_next = x_hat + (t_next - t_hat) * d_cur
 
         # Apply 2nd order correction.
         if i < num_steps - 1:
             denoised = net(x_next, t_next, class_labels).to(ideally_float64)
-            d_prime = (x_next - denoised) / t_next
+            d_prime = (x_next - denoised) / (t_next * temperature)
             x_next = x_hat + (t_next - t_hat) * (0.5 * d_cur + 0.5 * d_prime)
 
     # Save image grid.
@@ -82,12 +88,31 @@ def generate_image_grid(
 
 #----------------------------------------------------------------------------
 
-def main():
+@click.command()
+@click.option('--outdir',                  help='Where to save the output images', metavar='DIR',                   type=str, required=True)
+
+@click.option('--gridw',                   help='Number of image columns', metavar='INT',                           type=click.IntRange(min=1), default=8, show_default=True)
+@click.option('--gridh',                   help='Number of image rows', metavar='INT',                              type=click.IntRange(min=1), default=8, show_default=True)
+
+# @click.option('--sigma_min',               help='Lowest noise level  [default: varies]', metavar='FLOAT',           type=click.FloatRange(min=0, min_open=True))
+# @click.option('--sigma_max',               help='Highest noise level  [default: varies]', metavar='FLOAT',          type=click.FloatRange(min=0, min_open=True))
+# @click.option('--rho',                     help='Time step exponent', metavar='FLOAT',                              type=click.FloatRange(min=0, min_open=True), default=7, show_default=True)
+
+# @click.option('--S_churn', 'S_churn',      help='Stochasticity strength', metavar='FLOAT',                          type=click.FloatRange(min=0), default=0, show_default=True)
+# @click.option('--S_min', 'S_min',          help='Stoch. min noise level', metavar='FLOAT',                          type=click.FloatRange(min=0), default=0, show_default=True)
+# @click.option('--S_max', 'S_max',          help='Stoch. max noise level', metavar='FLOAT',                          type=click.FloatRange(min=0), default='inf', show_default=True)
+@click.option('--S_noise', 'S_noise',      help='Stoch. noise inflation', metavar='FLOAT',                          type=float, default=1, show_default=True)
+
+@click.option('--temperature',             help='Temperature', metavar='FLOAT',                                     type=click.FloatRange(min=0), default=1, show_default=True)
+
+def main(outdir, **generate_kwargs):
     model_root = 'https://nvlabs-fi-cdn.nvidia.com/edm/pretrained'
-    generate_image_grid(f'{model_root}/edm-cifar10-32x32-cond-vp.pkl',   'cifar10-32x32.png',  num_steps=18) # FID = 1.79, NFE = 35
-    generate_image_grid(f'{model_root}/edm-ffhq-64x64-uncond-vp.pkl',    'ffhq-64x64.png',     num_steps=40) # FID = 2.39, NFE = 79
-    generate_image_grid(f'{model_root}/edm-afhqv2-64x64-uncond-vp.pkl',  'afhqv2-64x64.png',   num_steps=40) # FID = 1.96, NFE = 79
-    generate_image_grid(f'{model_root}/edm-imagenet-64x64-cond-adm.pkl', 'imagenet-64x64.png', num_steps=256, S_churn=40, S_min=0.05, S_max=50, S_noise=1.003) # FID = 1.36, NFE = 511
+    os.makedirs(outdir, exist_ok=True)
+    print(generate_kwargs)
+    generate_image_grid(f'{model_root}/edm-cifar10-32x32-cond-vp.pkl',   f'{outdir}/cifar10-32x32.png',  num_steps=18, **generate_kwargs) # FID = 1.79, NFE = 35
+    generate_image_grid(f'{model_root}/edm-ffhq-64x64-uncond-vp.pkl',    f'{outdir}/ffhq-64x64.png',     num_steps=40, **generate_kwargs) # FID = 2.39, NFE = 79
+    generate_image_grid(f'{model_root}/edm-afhqv2-64x64-uncond-vp.pkl',  f'{outdir}/afhqv2-64x64.png',   num_steps=40, **generate_kwargs) # FID = 1.96, NFE = 79
+    # generate_image_grid(f'{model_root}/edm-imagenet-64x64-cond-adm.pkl', f'{outdir}/imagenet-64x64.png', num_steps=256, S_churn=40, S_min=0.05, S_max=50, S_noise=1.003, **generate_kwargs) # FID = 1.36, NFE = 511
 
 #----------------------------------------------------------------------------
 
